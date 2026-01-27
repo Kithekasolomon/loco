900d0f146f7382bc1b1139c59153d603ef0259c9
solomon <kithekasolomon20@gmail.com>
2026-01-27 08:17:03 +0300

    changed base url back to localhost from IP

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000',
-  baseURL: 'http://192.168.3.37:5000',
+  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://192.168.3.37:5000',
 })
 
 api.interceptors.request.use((config) => {
